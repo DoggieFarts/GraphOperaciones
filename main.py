d8b4eb4db45057d6da7c0c132ace341c6d5b33e8
David <agutierrezp1701@alumno.ipn.mx>
2023-04-22 13:30:11 -0600

    Grid de restricciones dinamicas y fx objetivo
    Se agrego pedir las restricciones y la funciób objetivo, falta serializar los entrys o en su defecto poner tantos entrys solo para los números y lo demás en label

--- main.py
+++ main.py
@@ -1,65 +1,39 @@
 import tkinter
 from tkinter import LEFT
 from tkinter import *
+from tkinter import messagebox
 
 top = tkinter.Tk()
 top.geometry('500x500')
-#L1 = tkinter.Label(top, text="Función objetivo")
-#L1.pack(side = LEFT)
-#E1 = Entry(top)
-#E1.pack(side = RIGHT)
+numero = 0
+def validacionNum(char):
+        return char.isdigit()
+validation = top.register(validacionNum)
 
-a = 2
+def obtenerNum():
+    numero = int(number_of_inputs.get())
 
-fObjetivo = tkinter.Frame(top)
-fRestricciones = tkinter.Frame(top)
+    for j in range(numero):
+      l=tkinter.Label(top,text="Restricciones" + str(j+1)).grid(row=j+3,column=0)
+      e=tkinter.Entry(top).grid(row=j+3,column=1)
 
-def rowSum():
-    a = a + 1
-    return a
-sumarRow = fObjetivo.register(rowSum)
-def only_numbers(char):
-    return char.isdigit()
-validationNumber = fObjetivo.register(only_numbers)
 
-def only_plus_or_minus(char):
-    valido = False
-    if(char == '+' or char == '-'):
-        valido = True
-    return valido
-validationSing = fObjetivo.register(only_plus_or_minus)
 
-def agregarRestriccion():
-    L1 = tkinter.Label(fObjetivo, text="Función objetivo")
-    L1.grid(row = a, column=0)
-    E1 = tkinter.Entry(fObjetivo)
-    E1.grid(row= a, column=1)
-    E2 = tkinter.Entry(fObjetivo)
-    E2.grid(row= a, column=2)
+labelNumRes=tkinter.Label(top,text="Número de restricciones: ")
+labelNumRes.grid(row=0,column=0)
+number_of_inputs = tkinter.Entry(top, validate="key", validatecommand=(validation,"%S"))
+number_of_inputs.grid(row=0,column=1)
 
-fObjetivo.pack(pady=10,padx=10)
-label1 = tkinter.Label(fObjetivo, text="Función objetivo: Z = ")
-label1.grid(row = 0, column = 0, padx= 5, pady= 5)
-# Entry para X1
-entry1 = tkinter.Entry(fObjetivo, width=5, validate="key", validatecommand=(validationNumber, '%S'))
-entry1.grid(row=0, column = 1)
-# Label para X1
-label2 = tkinter.Label(fObjetivo, text="X_1")
-label2.grid(row = 0, column = 2, padx= 5, pady= 5)
-# Entry para el signo
-entry2 = tkinter.Entry(fObjetivo, width=3, validate="key", validatecommand=(validationSing,'%S'))
-entry2.grid(row=0, column = 3)
-# Entry para X2
-entry3 = tkinter.Entry(fObjetivo, width=5, validate="key", validatecommand=(validationNumber, '%S'))
-entry3.grid(row=0, column = 4, padx=7)
-# Label para X2
-label4 = tkinter.Label(fObjetivo, text="X_2")
-label4.grid(row = 0, column = 5, padx= 5, pady= 5)
+obtenerNum = tkinter.Button(text="Obtener número de restricciones", command=obtenerNum)
+obtenerNum.grid(row=2,column=0)
 
-button1 = tkinter.Button(fObjetivo, text = "Agregar restricción", command=combine_funcs(agregarRestriccion(), sumarRow()))
-button1.grid(row=1, column=1, padx=5)
+labelFunObj=tkinter.Label(top,text="Función objetivo")
+labelFunObj.grid(row=1,column=0)
+entryFunObj= tkinter.Entry(top).grid(row=1, column=1)
 
-button2 = tkinter.Button(fObjetivo, text = "Quitar restricción")
-button2.grid(row = 1, column = 3)
+
+
+
+
 
 top.mainloop()
