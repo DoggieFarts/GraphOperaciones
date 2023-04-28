548ac57bb901e2b041323970701fc759180fbd35
David <agutierrezp1701@alumno.ipn.mx>
2023-04-28 17:41:09 -0600

    Restricciones limitadas a 3
    Restricciones limitadas a 3 con un entry y tokenizador (Falta ver como graficar, recomiendo pandas)

--- main.py
+++ main.py
@@ -6,28 +6,18 @@ import numpy as np
 
 top = tkinter.Tk()
 top.geometry('500x500')
-numero = 0
-e2 = 0
+
 def validacionNum(char):
         return char.isdigit()
 validation = top.register(validacionNum)
 
 
 
-def obtenerNum():
-
-    numero = int(number_of_inputs.get())
-
-    for j in range(numero):
-        l = tkinter.Label(top, text="Restricciones" + str(j + 1))
-        l.grid(row=j + 3, column=0)
-        e = tkinter.Entry(top, width=5).grid(row=j + 3, column=1)
-    e2 = l
-
-
 def calcular():
-    restriccion = e2.get()
-    print(restriccion)
+    valorRes1 = restriccion1.get()
+    valorRes2 = restriccion2.get()
+    valorRes3 = restriccion3.get()
+    print(valorRes1)
 
 
 
@@ -37,16 +27,26 @@ labelFunObj = tkinter.Label(top, text="Función objetivo")
 labelFunObj.grid(row=0, column=0)
 entryFunObj = tkinter.Entry(top).grid(row=0, column=1)
 
-labelNumRes=tkinter.Label(top,text="Número de restricciones: ")
+#Restriccion 1
+labelNumRes=tkinter.Label(top,text="Restricción numero 1: ")
 labelNumRes.grid(row=1,column=0)
-number_of_inputs = tkinter.Entry(top, validate="key", validatecommand=(validation,"%S"))
-number_of_inputs.grid(row=1,column=1)
+restriccion1 = tkinter.Entry(top)
+restriccion1.grid(row=1, column=1)
+#Restriccion 2
+labelNumRes=tkinter.Label(top,text="Restricción numero 2: ")
+labelNumRes.grid(row=2,column=0)
+restriccion2 = tkinter.Entry(top)
+restriccion2.grid(row=2, column=1)
+#Restriccion 3
+labelNumRes=tkinter.Label(top,text="Restricción numero 3: ")
+labelNumRes.grid(row=3,column=0)
+restriccion3 = tkinter.Entry(top)
+restriccion3.grid(row=3, column=1)
 
-obtenerNum = tkinter.Button(text="Obtener número de restricciones", command=obtenerNum)
-obtenerNum.grid(row=2,column=0)
+
 
 calcularButton = tkinter.Button(text="Calcular", command=calcular)
-calcularButton.grid(row=2,column=1)
+calcularButton.grid(row=4,column=0)
 
 
 
